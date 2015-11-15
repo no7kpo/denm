4d37c3148b5870daffeb9049627931f2996f5169
no7kpo <mgmillenium2002@gmail.com>
2015-11-15 19:01:42 -0300

    login con google

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,6 @@
 <?php
 //Alias para poder mostrar imágenes de productos en toda la aplicación
-Yii::setAlias('@product_pictures','http://localhost/relevando/backend/imagenes/productos');
+Yii::setAlias('@product_pictures','http://localhost/denm/backend/imagenes/productos');
 return [
 	'language' =>'en-US', 
 
